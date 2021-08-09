5403edeca44b57fd8a63084dedb7f16c0148994d
Keith <mutambirwa.keith@gmail.com>
2021-08-09 17:01:00 +0200

    feat: add get_email_attachment task to schedule-beat

--- backend/app/app/core/celery_app.py
+++ backend/app/app/core/celery_app.py
@@ -1,9 +1,11 @@
 from celery import Celery
+from celery.schedules import crontab
 
-# TODo change rabbitmq user and password from defaults
+# TODO change rabbitmq user and password from defaults
 celery_app = Celery("worker", broker="amqp://guest@queue//",
                     backend="rpc://guest@queue//")
 
+celery_app.conf.timezone = 'Africa/Johannesburg'
 
 celery_app.conf.task_routes = {
     "app.worker.test_celery": "main-queue",
@@ -17,6 +19,11 @@ celery_app.conf.beat_schedule = {
         'schedule': 30.0,
         'args': (16, 16)
     },
+    'get-email-attachments': {
+        'task': 'app.worker.get_email_attachments',
+        'schedule': crontab(minute='*/5'),
+        'args': None
+    },
 }
 
-celery_app.conf.timezone = 'Africa/Johannesburg'
+celery_app.autodiscover_tasks(packages=['app.worker'])
